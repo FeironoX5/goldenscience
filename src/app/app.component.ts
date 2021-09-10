8369f87f8a5c3ccc4387cbe7bd1d4828c8ac95e0
FeironoX5 <feironox5@gmail.com>
2021-09-10 15:58:02 +0300

    Payment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -25,17 +26,44 @@ export class AppComponent {
   courses = [
     {
       title: 'Маркетолог',
-      price: '3 490 руб/мес',
+      price: 20394,
       time: '6 месяцев'
-    }
+    },
+    {
+      title: 'Скаммер',
+      price: 10699,
+      time: '6 месяцев'
+    }, {
+      title: 'Скаммер',
+      price: 15200,
+      time: '6 месяцев'
+    }, {
+      title: 'Скаммер',
+      price: 15200,
+      time: '6 месяцев'
+    }, {
+      title: 'Скаммер',
+      price: 15200,
+      time: '6 месяцев'
+    }, {
+      title: 'Скаммер',
+      price: 15200,
+      time: '6 месяцев'
+    },
   ];
-  cart = [false];
+  cart = {
+    items: Array<boolean>(this.courses.length),
+    s: 0,
+    c: 0,
+  };
+
   newsNext() {
     this.newsIndex++;
     if (this.newsIndex >= this.news.length) {
       this.newsIndex = 0;
     }
   }
+
   newsPrev() {
     if (this.newsIndex == 0) {
       this.newsIndex = this.news.length - 1;
@@ -43,4 +71,33 @@ export class AppComponent {
       this.newsIndex--;
     }
   }
+
+  addItem(i: number) {
+    this.cart.c++;
+    this.cart.items[i] = true;
+    this.cart.s += this.courses[i].price;
+  }
+
+  declOfNum(n: number, text_forms: any) {
+    n = Math.abs(n) % 100;
+    const n1 = n % 10;
+    if (n > 10 && n < 20) {
+      return text_forms[2];
+    }
+    if (n1 > 1 && n1 < 5) {
+      return text_forms[1];
+    }
+    if (n1 == 1) {
+      return text_forms[0];
+    }
+    return text_forms[2];
+  }
+
+  removeItem(i: number) {
+    this.cart.c--;
+    this.cart.items[i] = false;
+    this.cart.s -= this.courses[i].price;
+
+  }
+
 }
